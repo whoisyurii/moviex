f85585d24be70d29adc33d4a09446746230d28b4
Yurii <yuriimashchuk@gmail.com>
2025-04-22 14:36:14 +0300

    Add ListEmptyComponent

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -103,6 +103,15 @@ const Search = () => {
             )}
           </>
         }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim() ? "No movies found" : "Search for a movie"}
+              </Text>
+            </View>
+          ) : null
+        }
       />
     </View>
   );
