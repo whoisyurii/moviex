6b9e0e54d75e366cc6df9f7e2a1fb868695084c4
Yurii <yuriimashchuk@gmail.com>
2025-04-24 13:33:07 +0300

    Fix bugs

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { icons } from "@/constants/icons";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
     </View>
   );
 };
